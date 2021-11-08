96b46e8fc2a5a56bae17bdddae1512e8ff3a7e0a
LeozeraImp <leonardo.imperatori12@gmail.com>
2021-11-08 18:25:49 -0300

    Back End Farmacia Gen

---
+++ farmacia_gen/src/main/java/br/org/generation/farmacia_gen/model/Categoria.java
@@ -0,0 +1,47 @@
+package br.org.generation.farmacia_gen.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotBlank(message = "O atributo Categoria nao pode ser Nulo ou Vazio!")
+	private String categoria;
+	
+	private String descricao;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+}
